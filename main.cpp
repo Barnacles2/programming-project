db61c9f5925f5f726d82b11a533197146fc67093
Barnacles2 <167401099+Barnacles2@users.noreply.github.com>
2024-05-23 13:07:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <string>
+//#include <vector>--not yet being used
 using namespace std;
 //setting the constant variables
 const int groups_count =3;
@@ -18,12 +20,34 @@ int main() {
 std::map<std::string, int>gender_count;
     gender_count["male"] =0;
     gender_count["female"] =0;
-
+//displays group class and how many students per group
     int total_students =0;
     for (int i = 0; i < groups_count; i++) {
         cout<<"Group " <<i+1 <<" has "<< students_per_group<<" students. \n";
+        //will output and request user to put in name of the student first then the gender
+        for (int j = 0; j<students_per_group; j++) {
+           string name;
+           cout<<"Enter name of student" <<j+1<<":";
+           cin>>name;
 
+           string gender;
+           cout<<"Enter gender (Male/Female) of student " << j + 1 << ": ";
+           cin>>gender;
+
+           if (gender=="male") {
+               gender_count["male"]++;
+           }else if (gender=="female") {
+               gender_count["female"]++;
+           }else {
+               cout<<"Invalid gender. Try again. \n";
+               j--;
+               continue;
+           }
+           total_students++;
+       }
     }
+
+    
 
 
 
