3a33ca9f5252ea3e6763505e0d52e4369c1919c6
Barnacles2 <167401099+Barnacles2@users.noreply.github.com>
2024-06-06 12:03:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <vector>
-
 using namespace std;
 
-// Setting the constant variables
+// setting the constant variables
 const int groups_count = 3;
 const int students_per_group = 50;
 const int sports_count = 4;
@@ -22,11 +20,11 @@ int main() {
     gender_count["male"] = 0;
     gender_count["female"] = 0;
 
-    // Displays group class and how many students per group
+    // displays group class and how many students per group
     int total_students = 0;
     for (int i = 0; i < groups_count; i++) {
         cout << "Group " << i + 1 << " has " << students_per_group << " students. \n";
-        // Will output and request user to put in name of the student first then the gender
+        // will output and request user to put in name of the student first then the gender
         for (int j = 0; j < students_per_group; j++) {
             string name;
             cout << "Enter name of student " << j + 1 << ": ";
@@ -47,51 +45,51 @@ int main() {
             }
             total_students++;
 
-            // Co-curricular activities
-            vector<string> sports = {"Rugby", "Athletics", "Swimming", "Soccer"};
-            vector<string> clubs = {"Journalism Club", "Red Cross Society", "AISEC", "Business Club", "Computer Science Club"};
-
+            // co-curricular activities
+            cout << "Enter co-curricular activities for student " << j + 1 << ":\n";
             int sport_count = 0;
             int club_count = 0;
-
-            cout << "Enter co-curricular activities for student " << j + 1 << ":\n";
             while (true) {
-                int choice;
-               cout << "1. Sporting activities";
-               cin >> choice;
-               cout << "\n2. Club/Societies activities";
-               cin >> choice;
+                cout << "Do you want to participate in a sporting activity? (1 = Yes, 2 = No)\n";
+                int sport_choice;
+                cin >> sport_choice;
 
-                if (choice == 1) {
-                    if (sport_count < 1) {
-                        cout << "Select a sporting activity:\n";
-                        for (int k = 0; k < sports_count; k++) {
-                            cout << k + 1 << ". " << sports[k] << "\n";
-                        }
-                        int sport_choice;
-                        cin >> sport_choice;
-                        sport_count++;
-                    } else {
-                        cout << "You have already selected a sporting activity.\n";
-                    }
-                } else if (choice == 2) {
-                    if (club_count < 2 || sport_count == 0) {
-                        cout << "Select a club/societies activity:\n";
-                        for (int k = 0; k < clubs_count; k++) {
-                            cout << k + 1 << ". " << clubs[k] << "\n";
-                        }
-                        int club_choice;
-                        cin >> club_choice;
-                        club_count++;
-                    } else {
-                        cout << "You have already selected the maximum number of club/societies activities.\n";
-                    }
+                if (sport_choice == 1) {
+                    cout << "Select a sporting activity:\n";
+                    cout << "1. Rugby\n";
+                    cout << "2. Athletics\n";
+                    cout << "3. Swimming\n";
+                    cout << "4. Soccer\n";
+                    int sport_activity;
+                    cin >> sport_activity;
+                    sport_count++;
                 } else {
-                    cout << "Invalid choice. Try again.\n";
+                    break;
                 }
 
-                if (sport_count > 0 || club_count > 0) {
-                    break;
+                if (sport_count > 0) {
+                    cout << "Do you want to participate in a club/societies activity? (1 = Yes, 2 = No)\n";
+                    int club_choice;
+                    cin >> club_choice;
+
+                    if (club_choice == 1) {
+                        cout << "Select a club/societies activity:\n";
+                        cout << "1. Journalism Club\n";
+                        cout << "2. Red Cross Society\n";
+                        cout << "3. AISEC\n";
+                        cout << "4. Business Club\n";
+                        cout << "5. Computer Science Club\n";
+                        int club_activity;
+                        cin >> club_activity;
+                        club_count++;
+                    } else {
+                        break;
+                    }
+
+                    if (club_count > 2) {
+                        cout << "You have already selected two club/societies activities. No more activities can be added.\n";
+                        break;
+                    }
                 }
             }
         }
@@ -99,3 +97,4 @@ int main() {
 
     return 0;
 }
+    
