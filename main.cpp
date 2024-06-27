b775f4e54abafd155661df774a068c5c56d0b796
Barnacles2 <167401099+Barnacles2@users.noreply.github.com>
2024-06-27 19:56:07 +0300

    Update main.cpp
    This is the last update the most basic way to make the code.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
-#include <map>
 #include <string>
-#include <vector>
+
 using namespace std;
 
-// setting the constant variables
 const int groups_count = 3; // number of groups
 const int students_per_group = 50; // number of students per group
 const int sports_count = 4; // number of sporting activities
 const int clubs_count = 5; // number of club/societies activities
 const int sports_max_capacity = 20; // maximum capacity for each sporting activity
 const int clubs_max_capacity = 60; // maximum capacity for each club/societies activity
-const int sports_min_members = 1; // minimum number of members required for each sporting activity
-const int clubs_min_members = 1; // minimum number of members required for each club/societies activity
-const int sports_max_male_percentage = 75; // maximum percentage of male members allowed for each sporting activity
-const int clubs_max_male_percentage = 50; // maximum percentage of male members allowed for each club/societies activity
 
-// define a struct to represent a student
 struct Student {
     string firstname;
     string surname;
@@ -27,18 +20,9 @@ struct Student {
     string club;
 };
 
-// vector to store all students
-vector<Student> students;
+Student students[150]; // array to store all students
+int student_count = 0; // count of students
 
-// maps to store the count of members for each sporting activity and club/society
-map<string, int> sports_members;
-map<string, int> clubs_members;
-
-// maps to store the count of male members for each sporting activity and club/society
-map<string, int> sports_male_members;
-map<string, int> clubs_male_members;
-
-// function to add a new student
 void addStudent() {
     Student student;
     cout << "Enter firstname: ";
@@ -79,149 +63,89 @@ void addStudent() {
             return;
     }
     student.sport = sport_name;
-    // update the sports_members map
-    sports_members[sport_name]++;
-    if (student.gender == "male") {
-        sports_male_members[sport_name]++;
-    }
 
     // add option to choose a club
     cout << "Choose a club (1-5): \n";
-    cout << "1. Debate\n";
-    cout << "2. Music\n";
-    cout << "3. Drama\n";
-    cout << "4. Art\n";
-    cout << "5. Robotics\n";
+    cout << "1. Journalism Club\n";
+    cout << "2. Red Cross Society\n";
+    cout << "3. AISEC\n";
+    cout << "4. Business Club\n";
+    cout << "5. Computer Science Club\n";
     int club_choice;
     cin >> club_choice;
     string club_name;
     switch (club_choice) {
         case 1:
-            club_name = "Debate";
+            club_name = "Journalism Club";
             break;
         case 2:
-            club_name = "Music";
+            club_name = "Red Cross Society";
             break;
         case 3:
-            club_name = "Drama";
+            club_name = "AISEC";
             break;
         case 4:
-            club_name = "Art";
+            club_name = "Business Club";
             break;
         case 5:
-            club_name = "Robotics";
+            club_name = "Computer Science Club";
             break;
         default:
             cout << "Invalid club choice. Try again.\n";
             return;
     }
     student.club = club_name;
-    // update the clubs_members map
-    clubs_members[club_name]++;
-    if (student.gender == "male") {
-        clubs_male_members[club_name]++;
-    }
 
-    students.push_back(student);
+    students[student_count] = student;
+    student_count++;
 }
 
-// function to view all students or students by group
 void viewStudents() {
     cout << "Viewing all students:\n";
-    for (const auto& student : students) {
-        cout << "Firstname: " << student.firstname << ", Surname: " << student.surname << ", Gender: " << student.gender << ", Age: " << student.age << ", Group: " << student.group << ", Sport: " << student.sport << ", Club: " << student.club << "\n";
+    for (int i = 0; i < student_count; i++) {
+        cout << "Firstname: " << students[i].firstname << ", Surname: " << students[i].surname << ", Gender: " << students[i].gender << ", Age: " << students[i].age << ", Group: " << students[i].group << ", Sport: " << students[i].sport << ", Club: " << students[i].club << "\n";
     }
     cout << "Viewing students by group:\n";
-    for (int i = 1;i <= groups_count; i++) {
+    for (int i = 1; i <= groups_count; i++) {
         cout << "Group " << i << ":\n";
-        for (const auto& student : students) {
-            if (student.group == i) {
-                cout << "Firstname: " << student.firstname << ", Surname: " << student.surname << ", Gender: " << student.gender << ", Age: " << student.age << ", Sport: " << student.sport << ", Club: " << student.club << "\n";
+        for (int j = 0; j < student_count; j++) {
+            if (students[j].group == i) {
+                cout << "Firstname: " << students[j].firstname << ", Surname: " << students[j].surname << ", Gender: " << students[j].gender << ", Age: " << students[j].age << ", Sport: " << students[j].sport << ", Club: " << students[j].club << "\n";
             }
         }
     }
 }
 
-// function to view available clubs and societies
 void viewClubsSocieties() {
     cout << "Available clubs and societies:\n";
-    cout << "1. Debate\n";
-    cout << "2. Music\n";
-    cout << "3. Drama\n";
-    cout << "4. Art\n";
-    cout << "5. Robotics\n";
+    cout << "1. Journalism Club\n";
+    cout << "2. Red Cross Society\n";
+    cout << "3. AISEC\n";
+    cout << "4. Business Club\n";
+    cout << "5. Computer Science Club\n";
     cout << "Available capacity for each club/society: " << clubs_max_capacity << "\n";
 }
 
-// function to view available sporting activities
 void viewSports() {
-    cout << "Available sporting activities:\n";
+    cout << "Available sports:\n";
     cout << "1. Rugby\n";
     cout << "2. Athletics\n";
     cout << "3. Swimming\n";
     cout << "4. Soccer\n";
-    cout << "Available capacity for each sporting activity: " << sports_max_capacity << "\n";
-}
-
-// function to view students participating in a specific activity
-void viewGroupedStudents() {
-    cout << "Enter activity (1-4 for sporting activities, 5-9 for clubs/societies): ";
-    int activity;
-    cin >> activity;
-    string activity_name;
-    switch (activity) {
-        case 1:
-            activity_name = "Rugby";
-            break;
-        case 2:
-            activity_name = "Athletics";
-            break;
-        case 3:
-            activity_name = "Swimming";
-            break;
-        case 4:
-            activity_name = "Soccer";
-            break;
-        case 5:
-            activity_name = "Debate";
-            break;
-        case 6:
-            activity_name = "Music";
-            break;
-        case 7:
-            activity_name = "Drama";
-            break;
-        case 8:
-            activity_name = "Art";
-            break;
-        case 9:
-            activity_name = "Robotics";
-            break;
-default:
-            cout << "Invalid activity. Try again.\n";
-            return;
-    }
-    cout << "Students participating in " << activity_name << ":\n";
-    for (const auto& student : students) {
-        // check if the student is participating in the activity
-        // (this logic is not implemented, you need to add it)
-        cout << "Firstname: " << student.firstname << ", Surname: " << student.surname << "\n";
-    }
+    cout << "Available capacity for each sport: " << sports_max_capacity << "\n";
 }
 
 int main() {
     int choice;
-    while (true) {
-        cout << "Menu:\n";
-        cout << "1. Add Student\n";
-        cout << "2. View Students\n";
-        cout << "3. View Clubs/Societies\n";
-        cout << "4. View Sports\n";
-        cout << "5. View Grouped Students\n";
-        cout << "6. Save all Files\n";
-        cout << "7. Exit\n";
+    do {
+        cout << "1. Add a student\n";
+        cout << "2. View all students\n";
+        cout << "3. View students by group\n";
+        cout << "4. View clubs and societies\n";
+        cout << "5. View sports\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
         cin >> choice;
-
         switch (choice) {
             case 1:
                 addStudent();
@@ -230,23 +154,20 @@ int main() {
                 viewStudents();
                 break;
             case 3:
-                viewClubsSocieties();
+                // code to view students by group
                 break;
             case 4:
-                viewSports();
+                viewClubsSocieties();
                 break;
             case 5:
-                viewGroupedStudents();
+                viewSports();
                 break;
             case 6:
-                // save all files (not implemented)
+                cout << "Exiting...\n";
                 break;
-            case 7:
-                return 0;
             default:
                 cout << "Invalid choice. Try again.\n";
         }
-    }
-
+    } while (choice != 6);
     return 0;
 }
